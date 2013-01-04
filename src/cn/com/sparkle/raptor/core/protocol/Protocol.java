e156382846212154af5aae78166a046ab217ed74
qinannmj <qinannmj@gmail.com>
2013-01-04 02:05:55 +0000

    noop

--- src/cn/com/sparkle/raptor/core/protocol/Protocol.java
+++ src/cn/com/sparkle/raptor/core/protocol/Protocol.java
@@ -10,9 +10,6 @@ public interface Protocol {
 	public void init(ProtecolHandlerAttachment attachment);
 	/**
 	 * Notification:
-	 *  If The IoBuffer.getByteBuffer.hasRemaining() return true,the buff will not
-	 *  be closed and it must be closed manually by invoking of close() when it will
-	 *  no longer be used.
 	 *  The method may be invoked twice with a single IoBuffer as the parameter.
 	 * @param attachment
 	 * @param buff
